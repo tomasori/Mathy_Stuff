04ffa10f35450aef91c1d8eef5f2c95786565c23
Tomas Orihuela <tomas.orihuela@gmail.com>
2022-07-19 20:34:05 -0500

    changed from pt and vector to two points

--- line3D-plane-intersection.py
+++ line3D-plane-intersection.py
@@ -5,15 +5,16 @@
 from sympy import Plane, Line3D, Point3D
 import numpy as np
 
-def line3D_plane_intersection(pt_on_line, line_dir_vector, plane_joints):
-
+def line3D_plane_intersection(pt1, pt2, plane_joints):
+    ''' Returns a list with X,Y,Z coordinates of a line defined by two points and
+    and plane defined by three joints '''
     #plane Points
     a1 = Point3D (plane_joints[0])
     a2 = Point3D (plane_joints[1])
     a3 = Point3D (plane_joints[2])
     #line Points
-    p0 = Point3D (pt_on_line) #point in line
-    v0 = line_dir_vector #line direction as vector
+    p0 = Point3D (pt1) #point in line
+    v0 = [pt2[0]-pt1[0], pt2[1]-pt1[1], pt2[2]-pt1[2] ] #line direction as vector
 
     #create plane and line
     plane = Plane(a1,a2,a3)
@@ -33,12 +34,12 @@ def line3D_plane_intersection(pt_on_line, line_dir_vector, plane_joints):
 
 # define elevation of plane
 elev = 10
-#plane Points
-plane_joints = [[-10000,-10000,elev],
-                [10000,-10000,elev],
-                [10000,10000,elev]]
-#line Points
-pt_on_line = [0,0,-50] #point in line
-line_dir_vector = [0, 1 ,1] #line direction as vector
+# plane Points
+plane_joints = [[-10000, -10000, elev],
+                [10000, -10000, elev],
+                [10000, 10000, elev]]
+# 3D points defining line
+pt1 = [0,0,-50]
+pt2 = [0, 30.333 ,100]
 
-print(line3D_plane_intersection(pt_on_line, line_dir_vector, plane_joints))
+print(line3D_plane_intersection(pt1, pt2, plane_joints))
